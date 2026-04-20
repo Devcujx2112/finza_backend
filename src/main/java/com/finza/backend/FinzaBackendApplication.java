7d090c63c14d67962e493a4330dc17756010c10c
Tùng Dương <Vutungduongnight@gmail.com>
2026-04-20 18:09:03 +0700

    first commit

---
+++ src/main/java/com/finza/backend/FinzaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finza.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinzaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinzaBackendApplication.class, args);
+    }
+
+}
